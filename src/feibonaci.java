14545c961288d80891461759e5254b1a2bdb7114
曹伟锴 <14490817+cao-weikai@user.noreply.gitee.com>
2024-06-18 21:15:40 +0800

    第一次

--- src/feibonaci.java
+++ src/feibonaci.java
@@ -1,4 +1,5 @@
 
+import java.util.HashMap;
 import java.util.Scanner;
 //斐波那契数列，输入一个大于10的整数，打印出一个大于它的最小斐波那契数
 public class feibonaci {
@@ -13,11 +14,18 @@ public class feibonaci {
         System.out.println("第"+n+"个斐波数"+"="+feiboncci(n));
 }
 //斐波那契数列
-public  static int feiboncci(int n){
-        if (n==1||n==2){
-            return 1;
-        }
-        return feiboncci(n-1)+feiboncci(n-2);
+public static HashMap<Integer,Integer>map =new HashMap<>();
+public  static int feiboncci(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    } else if (map.containsKey(n)) {
+        return map.get(n);
     }
-}
+      else {
+            int fn = feiboncci(n-1)+feiboncci(n-2);
+            map.put(n,fn);
+            return fn;
+        }
+
+}}
 
